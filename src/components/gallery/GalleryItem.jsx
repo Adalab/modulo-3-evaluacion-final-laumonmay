34a56aed282384e8a5cebff296e484d32ea18d8e
laumonmay <laumonmay@hotmail.com>
2024-02-15 21:42:46 +0100

    add styles

--- src/components/gallery/GalleryItem.jsx
+++ src/components/gallery/GalleryItem.jsx
@@ -2,17 +2,29 @@ import PropTypes from "prop-types";
 
 function GalleryItem({ character }) {
   return (
-    <>
-      <img
-        className="gallery__image"
-        src={character.image}
-        alt={character.name}
-      />
+    <div className="gallery__b">
+    
+      
+      <figcaption className="gallery__a">
+        <img
+            className="gallery__image"
+            src={character.image}
+            alt={character.name}
+        />
+      </figcaption>
+      
+      
+      
+      <img className="gallery__frame" src="public\CARA_A.png" alt="" />
+
+
+      
       <div className="gallery__text">
         <h3 className="gallery__character">{character.name}</h3>
         <span className="gallery__info">{character.species}</span>
       </div>
-    </>
+    </div>
+    
   );
 }
 
